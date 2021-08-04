e5b64b853fd4ffa2aea4587a4f5bc010b5157037
katieagne <katieagne@gmail.com>
2021-08-04 16:19:07 -0700

    add logo

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import "../Header/header.scss";
+import "../../assets/Logo";
 
 const Header = () => {
   return (
     <section className="header">
       <nav className="header__nav">
         <article className="header__logo-container">
-          <img src="" />
+          <img src="../../assets/Logo/InStock-Logo_1x.png" />
         </article>
       </nav>
     </section>
