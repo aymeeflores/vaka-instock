59ed5bb23ac1e55772292c630bb8f3fb123f0b47
aymeeflores <aymeeflores@users.noreply.github.com>
2021-08-05 15:07:28 -0400

    started rendering component

--- client/src/components/WarehouseDetails/WarehouseDetails.js
+++ client/src/components/WarehouseDetails/WarehouseDetails.js
@@ -4,9 +4,34 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1>Inventory</h1>
-        <textarea></textarea>
-        <button></button>
+        <section>
+          <h1>King West</h1>
+          <h4>warehouse address:</h4>
+          <address>469 King Street West, Tronto, CAN</address>
+          <div>
+            <h4>contact name</h4>
+            <p>Graeme Lyon</p>
+            <p>Warehouse Manager</p>
+          </div>
+          <div>
+            <h4>contact information:</h4>
+            <p>glyon@instock.com</p>
+          </div>
+        </section>
+        <section>
+          <h4>Inventory item</h4>
+          <p>television</p>
+          <h4>category</h4>
+          <p>Electronics</p>
+          <h4>Status</h4>
+          <p>in stock</p>
+          <h4>qty</h4>
+          <p>500</p>
+          <section>
+            <button>delete</button>
+            <button>edit</button>
+          </section>
+        </section>
       </>
     );
   }
