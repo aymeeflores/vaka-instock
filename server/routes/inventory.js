3115d6d451efab4ee941334af7115d84e553c9f0
katieagne <katieagne@gmail.com>
2021-08-04 23:31:54 -0700

    create function to get data and get single item

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,4 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+// function to get inventory data from JSON file
+const getInventoryData = () => {
+  const inventoryData = fs.readFileSync("./data/inventories.json");
+  const parsedInventoryData = JSON.parse(inventoryData);
+  return parsedInventoryData;
+};
+
+// get list of all inventory items
+
+/* AYMEE UNCOMMENT UR WELCOMEEEE
+router.get("/", (req, res) => {
+  const inventories = getInventoryData();
+  res.json(inventories);
+});
+*/
+
+// get single inventory item
+router.get("/:id", (req, res) => {
+  const inventories = getInventoryData();
+  const singleItem = inventories.find((item) => item.id === req.params.id);
+  res.send(singleItem);
+});
+
+// post new inventory item
 
 module.exports = router;
