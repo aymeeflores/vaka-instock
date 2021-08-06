ff60acdd7e7110748c01e15503cfcdec4ad2d3ad
katieagne <katieagne@gmail.com>
2021-08-05 18:58:27 -0700

    create responsive styles for list container

--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import searchIcon from "../../assets/Icons/search-24px.svg";
 import "../InventoryList/inventoryList.scss";
+import { Link } from "react-router-dom";
 
 const InventoryList = () => {
   return (
@@ -9,14 +10,24 @@ const InventoryList = () => {
         <h1 className="inventory__page-title">Inventory</h1>
         <article className="inventory__search-btn-container">
           <article className="inventory__search-wrapper">
+            <img
+              className="inventory__search-icon"
+              src={searchIcon}
+              alt="magnifying glass icon"
+            />
             <input
+              className="inventory__search"
               type="text"
               name="search"
-              id="seatch"
+              id="search"
               placeholder="Search..."
             />
-            <img src={searchIcon} alt="magnifying glass icon" />
           </article>
+          <Link className="inventory__btn-link" to="">
+            <article className="inventory__btn">
+              <p className="inventory__btn-text">+ Add New Item</p>
+            </article>
+          </Link>
         </article>
       </article>
     </article>
