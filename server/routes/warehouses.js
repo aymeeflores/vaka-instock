45bea17079a597a34731ff189718ac63114d1dae
ARoibu <aroibu@uwo.ca>
2021-08-08 00:06:45 -0400

    created and successfully tested post request. working on validation and error response next

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -7,6 +7,12 @@ const uniqId = require("uniqid")
 currentdir = __dirname
 const warehouses = JSON.parse(fs.readFileSync(path.resolve(currentdir, "../data/warehouses.json")))
 
+const getWarehouseData = () => {
+    const warehouseData = fs.readFileSync("./data/warehouses.json");
+    const parsedWarehouseData = JSON.parse(warehouseData);
+    return parsedWarehouseData;
+  };
+
 router.get("/", (req, res) => {
     res.send(warehouses)
     console.log(warehouses)
@@ -41,4 +47,25 @@ router.put("/:id", (req, res) => {
     console.log(warehouses)
     fs.writeFileSync((path.resolve(currentdir, "../data/warehouses.json")), JSON.stringify(warehouses))
 })
+
+router.post("/", (req, res) => {
+    const warehouses = getWarehouseData();
+    let newWarehouse = {
+        id: uniqId(),
+        name: req.body.name,
+        address: req.body.address,
+        city: req.body.city,
+        country: req.body.country,
+        contact: {
+            name: req.body.contact.name,
+            position: req.body.contact.position,
+            phone: req.body.contact.phone,
+            email: req.body.contact.email
+        }
+    }
+    warehouses.push(newWarehouse);
+    fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouses));
+    res.send(warehouses);
+})
+
 module.exports = router;
