65a614b66bb890a8a566260352da909915856ef7
aymeeflores <aymeeflores@users.noreply.github.com>
2021-08-09 01:04:13 -0400

    conected backend to warehousdetails

--- client/src/pages/Warehouses/Warehouses.js
+++ client/src/pages/Warehouses/Warehouses.js
@@ -1,56 +1,57 @@
-import React from 'react'
-import { API_URL } from '../../util'
-import axios from "axios"
-import WarehouseList from '../../components/WarehouseList/WarehouseList';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './Warehouses.scss'
-import DeleteWarehouse from '../../components/DeleteWarehouse/DeleteWarehouse';
+import React from "react";
+import { API_URL } from "../../util";
+import axios from "axios";
+import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./Warehouses.scss";
+import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+import DeleteWarehouse from "../../components/DeleteWarehouse/DeleteWarehouse";
 
 class Warehouses extends React.Component {
-    state = {
-        warehouseList: [],
-        show: false
-    };
+  state = {
+    warehouseList: [],
+    show: false,
+  };
 
-    showModal = () => {
+  showModal = () => {
+    this.setState({
+      show: true,
+    });
+  };
+  onDelete = (e) => {
+    this.setState({
+      show: false,
+    });
+  };
+  onCancel = (e) => {
+    this.setState({
+      show: false,
+    });
+  };
+
+  componentDidMount() {
+    axios
+      .get(`${API_URL}/warehouses`)
+      .then((response) => {
         this.setState({
-            show: true
-        })
-    }
-    onDelete = (e) => {
-        this.setState({
-            show: false
-        })
-    }
-    onCancel = (e) => {
-        this.setState({
-            show: false
-        })
-    }
+          warehouseList: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    componentDidMount() {
-        axios.get(`${API_URL}/warehouses`)
-            .then((response) => {
-                this.setState({
-                    warehouseList: response.data
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
-    render() {
-        return (
-            <div className='warehouses'>
-                {this.state.warehouseList && (
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path='/' exact>
-                                <WarehouseList warehouseList={this.state.warehouseList} />
-
-                            </Route>
-                            {/* <Route path="/warehouses/edit/:id"
+  render() {
+    return (
+      <div className="warehouses">
+        {this.state.warehouseList && (
+          <BrowserRouter>
+            <Switch>
+              <Route path="/" exact>
+                <WarehouseList warehouseList={this.state.warehouseList} />
+              </Route>
+              {/* <Route path="/warehouses/edit/:id"
                                 render={(routerParams) => {
                                     return (
                                  change the component to whatever your components called
@@ -61,18 +62,19 @@ class Warehouses extends React.Component {
                                 );
                                 }}>
                             </Route> */}
-                            {/* <Route path="/warehouses/details/:id"
-                                render={(routerParams) => {
-                                    return (
-                                 change the component to whatever your components called
-                                        <WarehouseDetails
-                                        {...routerParams}
-                                        warehouse={this.state.inventoryItems}
-                                      />
-                                );
-                                }}>
-                            </Route> */}
-                            {/* <Route path="/warehouses/add/:id"
+              <Route
+                path="/warehouses/details/:id"
+                render={(routerParams) => {
+                  return (
+                    // change the component to whatever your components called
+                    <WarehouseDetails
+                      {...routerParams}
+                      warehouse={this.state.inventoryItems}
+                    />
+                  );
+                }}
+              ></Route>
+              {/* <Route path="/warehouses/add/:id"
                                 render={(routerParams) => {
                                     return (
                                  change the component to whatever your components called
@@ -83,21 +85,23 @@ class Warehouses extends React.Component {
                                 );
                                 }}>
                             </Route> */}
-                            <Route path="/warehouses/delete/:id"
-                                render={(routerParams) => {
-                                    return (
-                                        <DeleteWarehouse
-                                            {...routerParams}
-                                            warehouse={this.state.inventoryItems}
-                                        />
-                                    );
-                                }}>
-                            </Route>
-                        </Switch>
-                    </BrowserRouter>)}
-            </div>
-        );
-    }
+              <Route
+                path="/warehouses/delete/:id"
+                render={(routerParams) => {
+                  return (
+                    <DeleteWarehouse
+                      {...routerParams}
+                      warehouse={this.state.inventoryItems}
+                    />
+                  );
+                }}
+              ></Route>
+            </Switch>
+          </BrowserRouter>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Warehouses;
