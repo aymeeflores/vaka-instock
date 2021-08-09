88fc5dfba73ed2bd33fe7260313db3bc5ddb4c05
katieagne <katieagne@gmail.com>
2021-08-09 02:04:14 -0700

    create responsiveness at all breakpoints

--- client/src/components/AddNewWarehouse/AddNewWarehouse.js
+++ client/src/components/AddNewWarehouse/AddNewWarehouse.js
@@ -25,134 +25,135 @@ const AddNewWarehouse = () => {
       },
     };
     createWarehouse(newWarehouse);
-    console.log(newWarehouse);
-    // createNewWarehouse(newWarehouse);
   }
 
-  console.log("this is inside render");
   return (
     <article className="nw__container">
       <article className="nw__top">
-        <img src={arrow} alt="" />
+        <img src={arrow} alt="arrow icon" />
         <h1 className="nw__page-title">Add New Warehouse</h1>
       </article>
       <article className="nw__form-container">
         <form className="nw__form" onSubmit={handleSubmit}>
           {/* begin warehouse details*/}
-          <article className="nw__warehouse">
-            <h2 className="nw__form-title">Warehouse Details</h2>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="name">
-                Warehouse Name
-              </label>
-              <input
-                className="nw__input"
-                type="text"
-                name="warehouse"
-                id="warehouse"
-                // value={warehouse.}
-                placeholder="Warehouse Name"
-              />
+          <article className="nw__flex-container">
+            <article className="nw__warehouse">
+              <h2 className="nw__form-title">Warehouse Details</h2>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="name">
+                  Warehouse Name
+                </label>
+                <input
+                  className="nw__input"
+                  type="text"
+                  name="warehouse"
+                  id="warehouse"
+                  // value={warehouse.}
+                  placeholder="Warehouse Name"
+                />
+              </article>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="address">
+                  Street Address
+                </label>
+                <input
+                  className="nw__input"
+                  type="text"
+                  name="address"
+                  id="address"
+                  // value="address"
+                  placeholder="Street Address"
+                />
+              </article>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="city">
+                  City
+                </label>
+                <input
+                  className="nw__input"
+                  type="text"
+                  name="city"
+                  id="city"
+                  // value="city"
+                  placeholder="City"
+                />
+              </article>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="country">
+                  Country
+                </label>
+                <input
+                  className="nw__input"
+                  type="text"
+                  name="country"
+                  id="country"
+                  // value="country"
+                  placeholder="Country"
+                />
+              </article>
             </article>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="address">
-                Street Address
-              </label>
-              <input
-                className="nw__input"
-                type="text"
-                name="address"
-                id="address"
-                // value="address"
-                placeholder="Street Address"
-              />
-            </article>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="city">
-                City
-              </label>
-              <input
-                className="nw__input"
-                type="text"
-                name="city"
-                id="city"
-                // value="city"
-                placeholder="City"
-              />
-            </article>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="country">
-                Country
-              </label>
-              <input
-                className="nw__input"
-                type="text"
-                name="country"
-                id="country"
-                // value="country"
-                placeholder="Country"
-              />
-            </article>
-          </article>
-          {/* begin contact details */}
-          <article className="nw__contact">
-            <h2 className="nw__form-title">Contact Details</h2>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="name">
-                Contact Name
-              </label>
-              <input
-                className="nw__input"
-                type="text"
-                name="contactName"
-                id="contactName"
-                // value={this.state.contactName}
-                placeholder="Contact Name"
-              />
-            </article>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="position">
-                Position
-              </label>
-              <input
-                className="nw__input"
-                type="text"
-                name="position"
-                id="position"
-                // value={this.state.position}
-                placeholder="Position"
-              />
-            </article>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="phone">
-                Phone Number
-              </label>
-              <input
-                className="nw__input"
-                type="tel"
-                name="phone"
-                id="phone"
-                // value={this.state.phone}
-                placeholder="Phone Number"
-              />
-            </article>
-            <article className="nw__input-container">
-              <label className="nw__input-lable" htmlFor="Email">
-                Email
-              </label>
-              <input
-                className="nw__input"
-                type="email"
-                name="email"
-                id="email"
-                // value={this.state.email}
-                placeholder="Email"
-              />
+            {/* begin contact details */}
+            <article className="nw__contact">
+              <h2 className="nw__form-title">Contact Details</h2>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="name">
+                  Contact Name
+                </label>
+                <input
+                  className="nw__input"
+                  type="text"
+                  name="contactName"
+                  id="contactName"
+                  // value={this.state.contactName}
+                  placeholder="Contact Name"
+                />
+              </article>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="position">
+                  Position
+                </label>
+                <input
+                  className="nw__input"
+                  type="text"
+                  name="position"
+                  id="position"
+                  // value={this.state.position}
+                  placeholder="Position"
+                />
+              </article>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="phone">
+                  Phone Number
+                </label>
+                <input
+                  className="nw__input"
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  // value={this.state.phone}
+                  placeholder="Phone Number"
+                />
+              </article>
+              <article className="nw__input-container">
+                <label className="nw__input-label" htmlFor="Email">
+                  Email
+                </label>
+                <input
+                  className="nw__input"
+                  type="email"
+                  name="email"
+                  id="email"
+                  // value={this.state.email}
+                  placeholder="Email"
+                />
+              </article>
             </article>
           </article>
           <article className="nw__btn-container">
-            <button>Cancel</button>
-            <button type="submit">+ Add Warehouse</button>
+            <button className="nw__btn-cancel">Cancel</button>
+            <button className="nw__btn-submit" type="submit">
+              + Add Warehouse
+            </button>
           </article>
         </form>
       </article>
