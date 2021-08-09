5af7dd6cd75b523b7e8a3034f22f84cfcb59d2db
Vinco Wu <vwu13@outlook.com>
2021-08-09 16:49:27 -0400

    fixed error messages with warehouses.js routing

--- client/src/pages/Warehouses/Warehouses.js
+++ client/src/pages/Warehouses/Warehouses.js
@@ -5,49 +5,50 @@ import WarehouseList from '../../components/WarehouseList/WarehouseList';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './Warehouses.scss'
 import DeleteWarehouse from '../../components/DeleteWarehouse/DeleteWarehouse';
+import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails';
 class Warehouses extends React.Component {
-    state = {
-        warehouseList: [],
-        show: false
-    };
-    showModal = () => {
+  state = {
+    warehouseList: [],
+    show: false
+  };
+  showModal = () => {
+    this.setState({
+      show: true
+    })
+  }
+  onDelete = (e) => {
+    this.setState({
+      show: false
+    })
+  }
+  onCancel = (e) => {
+    this.setState({
+      show: false
+    })
+  }
+  componentDidMount() {
+    axios.get(`${API_URL}/warehouses`)
+      .then((response) => {
         this.setState({
-            show: true
-        })
-    }
-    onDelete = (e) => {
-        this.setState({
-            show: false
-        })
-    }
-    onCancel = (e) => {
-        this.setState({
-            show: false
-        })
-    }
-    componentDidMount() {
-        axios.get(`${API_URL}/warehouses`)
-            .then((response) => {
-                this.setState({
-                    warehouseList: response.data
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-    render() {
-        { console.log(this.state) }
-        return (
-            <div className='warehouses'>
-                {this.state.warehouseList && (
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path='/' exact>
-                                <WarehouseList warehouseList={this.state.warehouseList} deleteWindow={this.showModal} />
-                           
-                            </Route>
-                            {/* <Route path="/warehouses/edit/:id"
+          warehouseList: response.data
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    { console.log(this.state) }
+    return (
+      <div className='warehouses'>
+        {this.state.warehouseList && (
+          <BrowserRouter>
+            <Switch>
+              <Route path='/' exact>
+                <WarehouseList warehouseList={this.state.warehouseList} deleteWindow={this.showModal} />
+
+              </Route>
+              {/* <Route path="/warehouses/edit/:id"
                                 render={(routerParams) => {
                                     return (
                                  change the component to whatever your components called
@@ -58,19 +59,19 @@ class Warehouses extends React.Component {
                                 );
                                 }}>
                             </Route> */}
-                            <Route
-                                path="/warehouses/details/:id"
-                                render={(routerParams) => {
-                                    return (
-                                        // change the component to whatever your components called
-                                        <WarehouseDetails
-                                            {...routerParams}
-                                            warehouse={this.state.inventoryItems}
-                                        />
-                                    );
-                                }}
-                            ></Route>
-                            <Route
+              <Route
+                path="/warehouses/details/:id"
+                render={(routerParams) => {
+                  return (
+                    // change the component to whatever your components called
+                    <WarehouseDetails
+                      {...routerParams}
+                      warehouse={this.state.inventoryItems}
+                    />
+                  );
+                }}
+              ></Route>
+              {/* <Route
                                 path="/warehouses/add"
                                 exact
                                 render={(routerParams) => {
@@ -81,27 +82,27 @@ class Warehouses extends React.Component {
                                         />
                                     );
                                 }}
-                            ></Route>
-                            <Route path="/warehouses/delete/:id"
-                                render={(routerParams) => {
-                                    return (
-                                        <>
-                                            <WarehouseList warehouseList={this.state.warehouseList} deleteWindow={this.showModal} />
-                                            <DeleteWarehouse
-                                                {...routerParams}
-                                                show={this.state.show}
-                                                warehouse={this.state.inventoryItems}
-                                                delete={this.onDelete}
-                                                cancel={this.onCancel}
-                                            />
-                                        </>
-                                    );
-                                }}>
-                            </Route>
-                        </Switch>
-                    </BrowserRouter>)}
-            </div>
-        );
-    }
+                            ></Route> */}
+              <Route path="/warehouses/delete/:id"
+                render={(routerParams) => {
+                  return (
+                    <>
+                      <WarehouseList warehouseList={this.state.warehouseList} deleteWindow={this.showModal} />
+                      <DeleteWarehouse
+                        {...routerParams}
+                        show={this.state.show}
+                        warehouse={this.state.inventoryItems}
+                        delete={this.onDelete}
+                        cancel={this.onCancel}
+                      />
+                    </>
+                  );
+                }}>
+              </Route>
+            </Switch>
+          </BrowserRouter>)}
+      </div>
+    );
+  }
 }
 export default Warehouses;
